f90f08df506038e858d172b347778ca6736d6d55
Wafer091 <95607685+dorimisini@users.noreply.github.com>
2024-02-11 12:12:54 +0000

    Update mark.py

--- mark.py
+++ mark.py
@@ -1,18 +1,18 @@
-score = int(input("Insert your Score: "))
-
-if score == 50:
-    print("Grade = 4")
-elif score >= 50 and score < 60:
-    print("Grade = 5")
-elif score >= 60 and score < 70:
-    print("Grade = 6")
-elif score >= 70 and score < 80:
-    print("Grade = 7")
-elif score >= 80 and score < 90:
-    print("Grade = 8")
-elif score >= 90 and score < 95:
-    print("grade = 9")
-elif score >= 95 and score < 101:
-    print("Grade = 10")
-else:
-    print("Error")
+while True:
+    score = int(input("Insert your Score: "))
+    if score == 50:
+        print("Grade = 4")
+    elif 50 <= score < 60:
+        print("Grade = 5")
+    elif 60 <= score < 70:
+        print("Grade = 6")
+    elif 70 <= score < 80:
+        print("Grade = 7")
+    elif 80 <= score < 90:
+        print("Grade = 8")
+    elif 90 <= score < 95:
+        print("Grade = 9")
+    elif 95 <= score <= 100:
+        print("Grade = 10")
+    else:
+        print("Error")
